11db1ff9f11307e12c450e471d4489605efc769b
invitado <invitado@LAB010212.itchii.lab>
2022-04-26 23:44:11 -0600

    Se agrega proyecto

---
+++ src/datos/Datos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package datos;
+
+/**
+ *
+ * @author invitado
+ */
+public class Datos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Informacion info = new Informacion ("JGS","Sistemas",3);
+        //encapsulamiento
+        String NumCon = info.getNombre()*" "+info.getCarrera()+" "+info.getSemestre()+" "
+        System.out.println("/nDatos generales alumno(a) : "+NumCon);
+        System.out.println("Hola");
+    }
+    // se crean metodos
+}
